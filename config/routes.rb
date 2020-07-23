245963f5b31bb2a8daad40d3489831d60dc726f7
shintarou.kawabe <s.kawabe2281@gmail.com>
2020-07-23 03:52:54 +0000

    signin&signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'artists/index'
-  get 'artists/show'
-  get 'artists/new'
-  get 'artists/edit'
   root to: 'artists#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
  
   resources :artists, only: [:index, :show, :new, :create, :edit, :update]
-  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :edit, :update] do
       member do
         get :pass, to: 'users#pass'
